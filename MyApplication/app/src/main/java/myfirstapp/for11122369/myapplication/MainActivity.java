0650f01f5816f3b737b71ea023bba93ebde433f2
David Forster <for11122369@COBC33366.CityBathColl.ac.uk>
2015-10-07 11:39:21 +0100

    new changes

--- MyApplication/app/src/main/java/myfirstapp/for11122369/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/myfirstapp/for11122369/myapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package myfirstapp.for11122369.myapplication;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -9,6 +12,9 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +24,11 @@ public class MainActivity extends AppCompatActivity {
     private Button btnTrue;
     private TextView lblQuestion;
     private ImageView imgPicture;
+
+    private List<QuestionObject> questions;
+    private QuestionObject currentQuestion;
+    private int index;
+    private int score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,22 +50,91 @@ public class MainActivity extends AppCompatActivity {
 
         imgPicture.setImageResource(R.drawable.dave);
 
-        //onclick listeners
+        index = 0;
+        score = 0;
 
         btnFalse.setOnClickListener(new View.OnClickListener() {
             @Override
-           public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "false!", Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                determineButtonPress(false);
+
             }
         });
 
         btnTrue.setOnClickListener(new View.OnClickListener() {
             @Override
-        public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "correct!!", Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                determineButtonPress(true);
             }
-
         });
 
+        generateQuestions();
+        setUpQuestion();
+
+    }  //onCreate
+
+    private void generateQuestions() {
+        questions = new ArrayList<>();
+
+        questions.add(new QuestionObject("Is the capital of England London?", true, R.drawable.england));
+        questions.add(new QuestionObject("Is the capital of Egypt Cairo?", true, R.drawable.egypt));
+        questions.add(new QuestionObject("Is the capital of Germany Berlin?", true, R.drawable.germany));
+        questions.add(new QuestionObject("Is the capital of Finland Helsinki?", true, R.drawable.finland));
+        questions.add(new QuestionObject("Is the capital of Spain France?", false, R.drawable.spain));
+        questions.add(new QuestionObject("Is the capital of Italy Rome?", true, R.drawable.italy));
+        questions.add(new QuestionObject("Is the capital of Romania Paris?", false, R.drawable.romania));
+        questions.add(new QuestionObject("Is the capital of Switzerland Bath?", false, R.drawable.switzerland));
+        questions.add(new QuestionObject("Is the capital of Nigeria Abuja?", true, R.drawable.nigeria));
+        questions.add(new QuestionObject("Is the capital of Australia Sydney?", false, R.drawable.australia));
+
     }
-}
+
+    private void setUpQuestion() {
+
+        if (index == questions.size()) {
+            // they've used all their questions time to end the game
+            Log.d("DANIELLE_APP", "ended all the questions");
+            endGame();
+        } else {
+
+
+            currentQuestion = questions.get(index);
+
+            lblQuestion.setText(currentQuestion.getQuestion());
+            imgPicture.setImageResource(currentQuestion.getPicture());
+            index++;
+        }
+    }
+
+    private void determineButtonPress(boolean answer) {
+        boolean expectedAnswer = currentQuestion.isAnswer();
+
+        if (answer == expectedAnswer) {
+            // you were right!
+        score ++;
+            Toast.makeText(MainActivity.this, "Correct!", Toast.LENGTH_SHORT).show();
+        } else {
+            // you were wrong!
+
+            Toast.makeText(MainActivity.this, "False!", Toast.LENGTH_SHORT).show();
+        }
+
+        setUpQuestion();
+
+    }
+private void endGame(){
+        final AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this)
+                .setTitle("Congratulations")
+                .setMessage("You scored " + score + " points this round!")
+                .setNeutralButton("ok", new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int which) {
+                        //don't do anything yet!
+                    }
+                })
+    .create();
+        alertDialog.show();
+
+
+    }
+
+} // end activity
